58ad997dc735cf7ede2022469ab64c427ca7fd40
Jörg <friebels.joerg@gmail.com>
2016-01-19 21:16:39 +0100

    .

---
+++ thegeocacher-ejb/src/main/java/thegeocacher/ejb/mapsforge/MapsforgeMapDownloader.java
@@ -0,0 +1,10 @@
+package thegeocacher.ejb.mapsforge;
+
+/**
+ *
+ * @author Jörg Friebel
+ * @since 19.01.2016 
+ */
+public class MapsforgeMapDownloader {
+
+}
---
+++ thegeocacher-ejb/src/main/java/thegeocacher/ejb/mapsforge/MapsforgeProperties.java
@@ -0,0 +1,13 @@
+package thegeocacher.ejb.mapsforge;
+
+/**
+ *
+ * @author Jörg Friebel
+ * @since 19.01.2016
+ */
+public class MapsforgeProperties
+{
+   public final static String DOWNLOAD_PATH = "http://download.mapsforge.org/maps/";
+   public final static String OFFLINE_PATH = "maps/";
+
+}
